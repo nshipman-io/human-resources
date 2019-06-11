071f761763889a46af4255f322d3a0b1fc2bae9c
Norman Shipman <norman@nshipman.io>
2019-06-11 18:51:09 -0400

    Setting up structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read() 
+
+setup(
+    name='human-resources',
+    version='0.1.0',
+    description='Commandline user management utility',
+    long_description=readme,
+    author='Norman',
+    author_email='norman@nshipman.io',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
